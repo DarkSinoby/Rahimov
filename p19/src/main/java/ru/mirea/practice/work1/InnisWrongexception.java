b9e50c38de94d647bb12643041408c2001212e9d
DarkSinoby <105012585+DarkSinoby@users.noreply.github.com>
2022-12-12 15:04:11 +0300

    Delete p19 directory

--- p19/src/main/java/ru/mirea/practice/work1/InnisWrongexception.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.practice.work1;
-
-public class InnisWrongexception extends Exception {
-    public InnisWrongexception(String message) {
-        super(message);
-    }
-}
-
--- p19/src/main/java/ru/mirea/practice/work2/FioiswrongException.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.practice.work2;
-
-public class FioiswrongException extends Exception {
-    public FioiswrongException(String message) {
-        super(message);
-    }
-}
-
